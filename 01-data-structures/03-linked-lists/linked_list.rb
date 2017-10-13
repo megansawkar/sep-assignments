ff527afcf40cb1a1061373cdad3df9d9ff5571b6
megansawkar <megan.sawkar@gmail.com>
2017-10-12 22:00:51 -0700

    03-linked-lists update 10_12_17

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -4,39 +4,70 @@ class LinkedList
   attr_accessor :head
   attr_accessor :tail
 
+def initialize
+  @head = nil
+  @tail = nil
+end
+
+  def empty?
+    @head.nil? && @tail.nil?
+  end
+
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
-    linked_list = Node.new(@data)
-
-    @head = node.first
-    @tail = node.last
-
-    while linked_list != nil
-      node.next = node
-    end 
-
-
-
-    #while or some other loop, node does not equal nil, eval, node.next = Node.new()
+    new_node = Node.new(node) #create a new node with node input
+    if empty?
+      @head = @tail = node
+    else @tail.next = node
+      @tail = node
+    #  @size += 1
+    end
   end
+
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    until @head.next == @tail
+      @head = @head.next
+    end
+    @tail = nil
   end
 
   # This method prints out a representation of the list.
   def print
+     current_node = @head
+     while current_node != nil
+       puts "#{current_node.data}"
+       current_node = current_node.next
+     end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if @head == node
+      @head = node.next
+    #node = @head
+    #until node.next == @tail
+    #  node = node.next
+    #end
+    #node.next = nil
+    #@tail = node
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    new_node = Node.new(node)
+    if empty?
+      @head = @tail = node
+    else node.next = @head
+      @head = node
+    #  @size =+1
+    end
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+
   end
 end
--- 01-data-structures/03-linked-lists/node.rb
+++ 01-data-structures/03-linked-lists/node.rb
@@ -1,6 +1,6 @@
 class Node
-  attr_accessor :next
-  attr_accessor :data
+  attr_accessor :next #stores the next node in the linked list
+  attr_accessor :data #stores the value of the current list element 
 
   def initialize(data)
     @data = data
